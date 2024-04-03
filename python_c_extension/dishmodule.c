3a7ba48015dec1737ea9064367918a486c854fd7
tournesol59 <frederic.peugny@gmail.com>
2024-04-03 15:19:55 +0200

    April, 3rd

--- python_c_extension/dishmodule.c
+++ python_c_extension/dishmodule.c
@@ -4,33 +4,54 @@
 // fred: momentan it is exactly the same as spammodule.c
 // ...
 
-static PyMethodDef SpamMethods[] = {
-    {"system",  spam_system, METH_VARARGS,
+static PyMethodDef DishMethods[] = {
+    {"system",  dish_system, METH_VARARGS,
      "Execute a shell command."},
     {NULL, NULL, 0, NULL}        /* Sentinel */
 };
 
 static struct PyModuleDef spammodule = {
     PyModuleDef_HEAD_INIT,
-    "spam",   /* name of module */
-    spam_doc, /* module documentation, may be NULL */
+    "Dish",   /* name of module */
+    dish_doc, /* module documentation, may be NULL */
     -1,       /* size of per-interpreter state of the module,
                  or -1 if the module keeps state in global variables. */
-    SpamMethods
+    DishMethods
 };
 
-PyMODINIT_FUNC PyInit_spam(void)
+PyMODINIT_FUNC PyInit_dish(void)
 {
-    return PyModule_Create(&spammodule);
+    return PyModule_Create(&dishmodule);
 }
 
-static PyObject *spam_system(PyObject *self, PyObject *args)
+static PyObject *dish_system(PyObject *self, PyObject *args)
 {
-    const char *command;
+    char command[60];
+    int i,k,l,m;
+    const char *name;
+    const char *format;
+    const char *input;
+    const char esc[2]=" ";
+    const char flag[2]="-i";
     int sts;
 
-    if (!PyArg_ParseTuple(args, "s", &command))
+    if (!PyArg_ParseTuple(args, "s", &name, &format, &input))
         return NULL;
+    k=strlen(name);
+    l=strlen(format);
+    m=strlen(input);
+    command=NULL;
+    strcat(command, (char*) name);
+    strcat(command, (char*) esc);
+    strcat(command, (char*) flag);
+    strcat(command, (char*) esc);
+    strcat(command, (char*) format);
+    strcat(command, (char*) esc);
+    strcat(command, (char*) input);
+    for (i=0; i < (60-k-4-l-1-m); i++) [
+        strcat(command, (char*) esc);
+    }
+    // now we call shell to execute the command in string command:
     sts = system(command);
     return PyLong_FromLong(sts);
 }
